a2eddb2003598e1b637389c6bb4a6a41e3fbfd97
rony <ronyfernandes19@gmail.com>
2022-04-06 21:40:10 +0530

    modified 4.java file

--- 4.java
+++ 4.java
@@ -2,5 +2,5 @@ Hi Ma'am, I learnt how to edit contents in vi editor.
 I need to learn now how to copy and paste the contents from google. 
 on 5th april, I learnt how to copy and paste the contents from google. (I made changes to this file in github editor)
 
-Edit has been made in the main branch
+Conflit resolved. Hurray!!!!
 
